90220349b26106993ac07ffddcde9ba475795e9e
Colt Frederickson <coltfred@gmail.com>
2023-12-18 08:18:28 -0700

    Code review

--- src/v5/attached.rs
+++ src/v5/attached.rs
@@ -14,52 +14,68 @@ pub struct AttachedDocument {
     pub edoc: IvAndCiphertext,
 }
 
-/// Construct an IronCore attached EDOC from the constituent parts.
-pub fn encode_attached_edoc(
-    AttachedDocument {
-        key_id_header,
-        edek,
-        edoc,
-    }: &AttachedDocument,
-) -> Result<Bytes> {
-    let key_id_header_bytes = key_id_header.write_to_bytes();
-    let encoded_edek = edek.write_to_bytes().expect("Writing to bytes is safe");
-    if encoded_edek.len() > u16::MAX as usize {
-        Err(Error::HeaderLengthOverflow(encoded_edek.len() as u64))
-    } else {
-        let len = encoded_edek.len() as u16;
+impl TryFrom<Vec<u8>> for AttachedDocument {
+    type Error = Error;
 
-        let result = [
-            key_id_header_bytes.as_ref(),
-            &len.to_be_bytes(),
-            &encoded_edek,
-            &edoc.0, // Note that the edoc is written without a header since it's an attached document.
-        ]
-        .concat();
-        Ok(result.into())
+    /// Breaks apart an attached edoc into its parts.
+    fn try_from(value: Vec<u8>) -> Result<Self> {
+        Bytes::from(value).try_into()
     }
 }
 
-/// Breaks apart an attached edoc into its parts.
-pub fn decode_attached_edoc(b: Bytes) -> Result<AttachedDocument> {
-    let (key_id_header, mut attached_document_with_header) =
-        key_id_header::decode_version_prefixed_value(b)?;
-    if attached_document_with_header.len() > 2 {
-        let len = attached_document_with_header.get_u16();
-        if attached_document_with_header.len() > len as usize {
-            let header_bytes = attached_document_with_header.split_to(len as usize);
-            let edek = protobuf::Message::parse_from_bytes(&header_bytes[..])
-                .map_err(|e| Error::HeaderParseErr(e.to_string()))?;
-            Ok(AttachedDocument {
-                key_id_header,
-                edek,
-                edoc: IvAndCiphertext(attached_document_with_header),
-            })
+impl TryFrom<Bytes> for AttachedDocument {
+    type Error = Error;
+
+    /// Breaks apart an attached edoc into its parts.
+    fn try_from(value: Bytes) -> Result<Self> {
+        let (key_id_header, mut attached_document_with_header) =
+            key_id_header::decode_version_prefixed_value(value)?;
+        if attached_document_with_header.len() > 2 {
+            let edek_len = attached_document_with_header.get_u16();
+            if attached_document_with_header.len() > edek_len as usize {
+                let header_bytes = attached_document_with_header.split_to(edek_len as usize);
+                let edek = protobuf::Message::parse_from_bytes(&header_bytes[..])
+                    .map_err(|e| Error::HeaderParseErr(e.to_string()))?;
+                Ok(AttachedDocument {
+                    key_id_header,
+                    edek,
+                    edoc: IvAndCiphertext(attached_document_with_header),
+                })
+            } else {
+                Err(Error::HeaderParseErr(
+                    "The EDEK you passed in was too short based on the length bytes.".to_string(),
+                ))
+            }
         } else {
-            Err(Error::HeaderParseErr("Edek length too long.".to_string()))
+            Err(Error::HeaderParseErr("Header is too short.".to_string()))
         }
-    } else {
-        Err(Error::HeaderParseErr("Header is too short.".to_string()))
+    }
+}
+
+impl AttachedDocument {
+    /// Write out the entire v5 attached documents to bytes.
+    pub fn write_to_bytes(&self) -> Result<Bytes> {
+        let AttachedDocument {
+            key_id_header,
+            edek,
+            edoc,
+        } = self;
+        let key_id_header_bytes = key_id_header.write_to_bytes();
+        let encoded_edek = edek.write_to_bytes().expect("Writing to bytes is safe");
+        if encoded_edek.len() > u16::MAX as usize {
+            Err(Error::HeaderLengthOverflow(encoded_edek.len() as u64))
+        } else {
+            let len = encoded_edek.len() as u16;
+
+            let result = [
+                key_id_header_bytes.as_ref(),
+                &len.to_be_bytes(),
+                &encoded_edek,
+                &edoc.0, // Note that the edoc is written without the leading OIRON since it's an attached document.
+            ]
+            .concat();
+            Ok(result.into())
+        }
     }
 }
 
@@ -78,7 +94,7 @@ mod test {
         Error,
     };
 
-    use super::{decode_attached_edoc, encode_attached_edoc, AttachedDocument};
+    use super::*;
 
     #[test]
     fn test_roundtrip() {
@@ -111,8 +127,8 @@ mod test {
             edek,
             edoc,
         };
-        let encoded = encode_attached_edoc(&expected_result).unwrap();
-        let result = decode_attached_edoc(encoded).unwrap();
+        let encoded = expected_result.write_to_bytes().unwrap();
+        let result: AttachedDocument = encoded.try_into().unwrap();
         assert_eq!(result, expected_result);
     }
 
@@ -121,20 +137,21 @@ mod test {
         // The `0,255` after the `0` is the u16 representing length. It is too large.
         let encoded = vec![
             255u8, 255, 255, 255, 2, 0, 0, 255, 18, 7, 18, 5, 26, 3, 18, 1, 42, 100, 200,
-        ]
-        .into();
-        let result = decode_attached_edoc(encoded).unwrap_err();
+        ];
+        let result = AttachedDocument::try_from(encoded).unwrap_err();
         assert_eq!(
             result,
-            Error::HeaderParseErr("Edek length too long.".to_string())
+            Error::HeaderParseErr(
+                "The EDEK you passed in was too short based on the length bytes.".to_string()
+            )
         );
     }
 
     #[test]
     fn test_header_too_short() {
         // This value is just a key_id header with 1 0 after.
-        let encoded = vec![255u8, 255, 255, 255, 2, 0, 0].into();
-        let result = decode_attached_edoc(encoded).unwrap_err();
+        let encoded = vec![255u8, 255, 255, 255, 2, 0, 0];
+        let result = AttachedDocument::try_from(encoded).unwrap_err();
         assert_eq!(
             result,
             Error::HeaderParseErr("Header is too short.".to_string())
